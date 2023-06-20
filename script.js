830707e810632bb6f33f49783ccf03846683bebf
jaspreetScripts <119395932+jaspreetScripts@users.noreply.github.com>
2023-06-21 00:30:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const colorSelect = document.getElementById("colorSelect");
 const removeButton= document.getElementByTagName("input");
 
 removeButton.addEventListener('click',()=>{
-	const selectedOption = colorSelect.option[colorSelect.selectedIndex];
+	const selectedOption = colorSelect.options[colorSelect.selectedIndex];
 	colorSelect.removeChild(selectedOption);
 })
