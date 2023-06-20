49163950f8f5edd8d190dfd1d588b534a4beada9
jaspreetScripts <119395932+jaspreetScripts@users.noreply.github.com>
2023-06-21 00:23:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const colorSelect = document.getElementById("colorSelect");
+const removeButton= document.getElementByTagName("input");
+
+removeButton.addEventListener('click',()=>{
+	const selectedOption = colorSelect.option[colorSelect.selectedIndex];
+	colorSelect.removeChild(selectedOption);
+})
